e6f9a375c2eb41bfb0cc629c33e5796d8e44035e
Darryl-win <logossoudarryl20@gmail.com>
2024-07-15 14:43:35 +0000

    initialiation des routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:social_media_app/auth/login_or_register.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:social_media_app/pages/home_page.dart';
+import 'package:social_media_app/pages/profile_page.dart';
+import 'package:social_media_app/pages/users_page.dart';
 import 'package:social_media_app/themes/dark_mode.dart';
 import 'package:social_media_app/themes/light_mode.dart';
 import 'auth/auth.dart';
@@ -24,6 +27,12 @@ class MainApp extends StatelessWidget {
       darkTheme: darkMode,
       debugShowCheckedModeBanner: false,
       home: AuthPage(),
+      routes: {
+        '/login_register_page' : (context) => const LoginOrRegisterPage(),
+        '/home_page' : (context) => HomePage(),
+        '/profile_page' : (context) => const ProfilePage(),
+        '/users_page' : (context) => const UsersPage(),
+      },
     );
   }
 }
