4921eee42aded2e6e18e1178a4a4bbe512b478bd
Darryl-win L. <logossoudarryl20@gmail.com>
2024-09-16 11:30:10 +0000

    text Users

--- lib/pages/users_page.dart
+++ lib/pages/users_page.dart
@@ -7,7 +7,7 @@ class UsersPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Users>"),
+        title: Text("Users"),
       ),
     );
   }
